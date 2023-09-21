318bf06ab97e5189f6e0e0b68e813cc1821d0f07
mtssantos101 <mtssantos101@gmail.com>
2023-09-21 18:03:43 -0300

    Initial commit

---
+++ src/EstadoBrasileiro.java
@@ -0,0 +1,32 @@
+public enum EstadoBrasileiro {
+    SAO_PAULO ("SP","São Paulo",11),
+    RIO_JANEIRO ("RJ", "Rio de Janeiro",21),
+    PIAUI ("PI", "Piauí",86),
+    MARANHAO ("MA", "Maranhão",98),
+    BAHIA ("BA" ,"Bahia",71),
+    CEARA ("CE", "Ceará",85)
+    ;
+
+
+    private String nome;
+    private String sigla;
+    private int ibge;
+
+    private EstadoBrasileiro(String sigla, String nome, int ibge) {
+        this.sigla = sigla;
+        this.nome = nome;
+        this.ibge=ibge;
+    }
+    public int getIbge(){
+        return ibge;
+    }
+    public String getSigla() {
+        return sigla;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public String getNomeMaiusculo() {
+        return nome.toUpperCase();
+    }
+}
